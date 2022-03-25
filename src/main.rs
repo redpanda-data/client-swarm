a3e8a9971dc127031ab1652f24c0af320f09317b
John Spray <jcs@redpanda.com>
2022-03-25 10:27:19 +0000

    Add a license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// Copyright 2022 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
